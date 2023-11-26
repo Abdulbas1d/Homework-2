ac654569df452755b8d939abbf555f97e29da54a
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2023-11-27 00:13:01 +0500

    Example 1 is solved

---
+++ js/index.js
@@ -0,0 +1,9 @@
+1-Misol
+
+let n = +prompt("Birinchi sonning kiriting");
+let m = +prompt("Ikkinchi sonning kiriting");
+if (n > m) {
+    console.log(n ** 2);
+} else {
+    console.log(m ** 2);
+};
