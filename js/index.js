a7b49012f0820256dbd8b2c8a6b6720594576afc
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2023-11-27 00:19:58 +0500

    3 examples solved

--- js/index.js
+++ js/index.js
@@ -24,8 +24,15 @@
 //   } else {
 //     console.log(o);
 //   }
-// }
+// };
 
 
+// 3-Misol
 
+// let son = +prompt("Son kiriting");
 
+// if (son % 2 == 0) {
+//   console.log("Juft son");
+// } else {
+//   console.log("Toq son");
+// };
