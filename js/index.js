1caf1980e730d7a747c0a23807c362c1b0c3ba20
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2023-11-27 00:16:37 +0500

    2 examples solved

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
-1-Misol
+// 1-Misol
 
-let n = +prompt("Birinchi sonning kiriting");
-let m = +prompt("Ikkinchi sonning kiriting");
-if (n > m) {
-    console.log(n ** 2);
-} else {
-    console.log(m ** 2);
-};
+// let n = +prompt("Birinchi sonning kiriting");
+// let m = +prompt("Ikkinchi sonning kiriting");
+// if (n > m) {
+//     console.log(n ** 2);
+// } else {
+//     console.log(m ** 2);
+// };
+
+
+// 2-Misol
+
+// let m = +prompt("Birinchi sonni kiriting");
+// let n = +prompt("Ikkinchi sonni kiriting");
+// let o = +prompt("Uchinchi sonni kiriting");
+
+// if (n > m) {
+//   console.log(n);
+// } else {
+//   console.log(m);
+//   if (m > o) {
+//     console.log(m);
+//   } else {
+//     console.log(o);
+//   }
+// }
+
+
+
+
